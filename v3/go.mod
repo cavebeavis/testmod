b11774f68f1387cbcb18006b34397bff28c00c86
Brian <bkovacsconsulting@gmail.com>
2018-11-06 12:43:59 -0500

    v3.0.0

---
+++ v3/go.mod
@@ -0,0 +1,1 @@
+module github.com/cavebeavis/testmod/v3
---
+++ v3/testmod.go
@@ -0,0 +1,10 @@
+package testmod
+
+import (
+  "fmt"
+)
+
+//TestMe TODO: add
+func TestMe(name string) string {
+	return fmt.Sprintf("Hi, %s. \n\n You really suck!!!!!!!!!!!", name)
+}
